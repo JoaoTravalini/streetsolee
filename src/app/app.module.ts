1073bdb787a571ac2c5f484d76047ad77877fbb7
João Travalini <joao.m.travalini@gmail.com>
2025-03-30 15:15:02 -0300

    Criacao componentes autenticacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/autenticacao/login/login.component';
+import { CadastroComponent } from './components/autenticacao/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
