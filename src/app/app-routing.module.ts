2475b4e721b0e56596776fc3dddf21d56c21e6ec
João Travalini <joao.m.travalini@gmail.com>
2025-03-30 15:29:06 -0300

    Criacao de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/autenticacao/login/login.component';
+import { CadastroComponent } from './components/autenticacao/cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
