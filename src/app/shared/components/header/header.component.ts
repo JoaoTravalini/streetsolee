cae473a83541e2f5c32883bbbb9344043709fa62
João Travalini <joao.m.travalini@gmail.com>
2025-05-27 15:49:57 -0300

    Header fixo dinamico no scroll

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  previousScroll = 0;
+  isHidden = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScroll = window.scrollY || document.documentElement.scrollTop;
+
+    if (currentScroll > this.previousScroll && currentScroll > 100) {
+      this.isHidden = true;
+    }
+
+    if (currentScroll < this.previousScroll) {
+      this.isHidden = false;
+    }
+
+    this.previousScroll = currentScroll;
+  }
 }
